a684219e77330a9b08b470f36abe08b8cc425c4b
Outter Mia <outtermia@gmail.com>
2023-05-18 00:42:08 +0400

    Use CardTile to display cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
-import { Container } from '@mui/material';
+import {
+  Container,
+  ImageList,
+} from '@mui/material';
 import {
   Timeline,
   TimelineConnector,
@@ -9,6 +12,8 @@ import {
   TimelineSeparator,
 } from '@mui/lab';
 
+import CardTile from './CardTile'
+
 const EVENTS = [
   {
     title: 'Event 1',
@@ -17,21 +22,21 @@ const EVENTS = [
     cards: [
       {
         owned: false,
-        code: 'CMR',
+        code: 'cmr',
         number: 72,
         scryfallId: '',
         imgURL: 'https://cards.scryfall.io/large/front/a/7/a736df42-18e9-4259-b0b8-54d21143e72e.jpg?1608909239',
       },
       {
         owned: false,
-        code: 'CMR',
+        code: 'cmr',
         number: 42,
         scryfallId: '',
         imgURL: 'https://cards.scryfall.io/large/front/4/6/46975250-950f-4f5d-a7c5-f61aec8e40ad.jpg?1608908966',
       },
       {
         owned: false,
-        code: 'MH1',
+        code: 'mh1',
         number: 116,
         scryfallId: '',
         imgURL: 'https://cards.scryfall.io/large/front/8/6/8690cbcc-f8fd-41f7-9e28-e61c12b04014.jpg?1665611812',
@@ -45,14 +50,14 @@ const EVENTS = [
     cards: [
       {
         owned: false,
-        code: 'CMR',
+        code: 'cmr',
         number: 72,
         scryfallId: '',
         imgURL: 'https://cards.scryfall.io/large/front/a/7/a736df42-18e9-4259-b0b8-54d21143e72e.jpg?1608909239',
       },
       {
         owned: false,
-        code: 'CMR',
+        code: 'cmr',
         number: 42,
         scryfallId: '',
         imgURL: 'https://cards.scryfall.io/large/front/4/6/46975250-950f-4f5d-a7c5-f61aec8e40ad.jpg?1608908966',
@@ -61,22 +66,28 @@ const EVENTS = [
   },
 ];
 
-const Event = ({ title, date, description }: any) => (
-  <TimelineItem>
-    <TimelineOppositeContent>
-      <h3>{title}</h3>
-      <p>{date}</p>
-      <p>{description}</p>
-    </TimelineOppositeContent>
-    <TimelineSeparator>
-      <TimelineDot />
-      <TimelineConnector />
-    </TimelineSeparator>
-    <TimelineContent>
-      {/* Cards related to the event will go here */}
-    </TimelineContent>
-  </TimelineItem>
-);
+const Event = ({ title, date, description, cards }: any) => {
+  const CardTiles = cards.map((card: any) => <CardTile {...card} />)
+
+  return (
+    <TimelineItem>
+      <TimelineOppositeContent>
+        <h3>{title}</h3>
+        <p>{date}</p>
+        <p>{description}</p>
+      </TimelineOppositeContent>
+      <TimelineSeparator>
+        <TimelineDot />
+        <TimelineConnector />
+      </TimelineSeparator>
+      <TimelineContent>
+        <ImageList sx={{ width: 500 }}>
+          {CardTiles}
+        </ImageList>
+      </TimelineContent>
+    </TimelineItem>
+  )
+}
 
 function App() {
   const events = EVENTS.map((event) => <Event {...event} />);
