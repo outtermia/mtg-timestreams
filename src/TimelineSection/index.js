c352eeb0c36e77b82373581729ae07b514719bf9
Outter Mia <outtermia@gmail.com>
2023-07-26 14:36:47 +0400

    Add mocked data file and move events data there

--- src/TimelineSection/index.js
+++ src/TimelineSection/index.js
@@ -12,49 +12,10 @@ import {
   TimelineSeparator,
 } from '@mui/lab'
 
+import { EVENTS } from 'data/mock'
+
 import CardTile from './CardTile'
 
-const EVENTS = [
-  {
-    title: 'Event 1',
-    date: '2021-10-01',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    cards: [
-      {
-        owned: false,
-        code: 'cmr',
-        number: 72,
-      },
-      {
-        owned: false,
-        code: 'cmr',
-        number: 42,
-      },
-      {
-        owned: false,
-        code: 'mh1',
-        number: 116,
-      },
-    ],
-  },
-  {
-    title: 'Event 2',
-    date: '2021-10-02',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    cards: [
-      {
-        owned: false,
-        code: 'cmr',
-        number: 72,
-      },
-      {
-        owned: false,
-        code: 'cmr',
-        number: 42,
-      },
-    ],
-  },
-]
 
 const Event = ({ title, date, description, cards }: any) => {
   const CardTiles = cards.map((card: any) => <CardTile {...card} />)
---
+++ src/data/mock.js
@@ -0,0 +1,496 @@
+export const EVENTS = [
+  {
+    title: 'Event 1',
+    date: '2021-10-01',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    cards: [
+      {
+        owned: false,
+        code: 'cmr',
+        number: 72,
+      },
+      {
+        owned: false,
+        code: 'cmr',
+        number: 42,
+      },
+      {
+        owned: false,
+        code: 'mh1',
+        number: 116,
+      },
+    ],
+  },
+  {
+    title: 'Event 2',
+    date: '2021-10-02',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    cards: [
+      {
+        owned: false,
+        code: 'cmr',
+        number: 72,
+      },
+      {
+        owned: false,
+        code: 'cmr',
+        number: 42,
+      },
+    ],
+  },
+]
+
+export const DRAFT_EVENTS = [
+  {
+    year: 2021,
+    title: 'The Thran',
+    body: [],
+    cards: [
+      {
+        owned: false,
+        code: 'CMR',
+        number: 72,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/a/7/a736df42-18e9-4259-b0b8-54d21143e72e.jpg?1608909239',
+      },
+      {
+        owned: false,
+        code: 'CMR',
+        number: 42,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/4/6/46975250-950f-4f5d-a7c5-f61aec8e40ad.jpg?1608908966',
+      },
+      {
+        owned: false,
+        code: 'MH1',
+        number: 116,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/8/6/8690cbcc-f8fd-41f7-9e28-e61c12b04014.jpg?1665611812',
+      },
+    ]
+  },
+  {
+    year: 2021,
+    title: 'Null Moon',
+    body: '',
+    cards: [
+      {
+        owned: true,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/a/5/a5c7d16b-8f4e-42b9-be24-3cb091932d7c.jpg?1562740759',
+      },
+    ]
+  },
+  {
+    year: 2021,
+    title: 'Fall of the Thran',
+    body: '',
+    cards: [
+      {
+        owned: true,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/3/a/3a613a01-6145-4e34-987c-c9bdcb068370.jpg?1562734219',
+      },
+    ]
+  },
+  {
+    year: '10 AR - 20 AR',
+    title: 'Tocasia\'s Onulet',
+    body: '',
+    cards: [
+      {
+        owned: true,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/e/4/e4abf2fb-f78a-4e75-b46f-524d3d7405db.jpg?1674420424',
+      },
+    ]
+  },
+  {
+    year: 2021,
+    title: 'Tocasia\'s Welcome',
+    body: '',
+    cards: [
+      {
+        owned: true,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/5/6/56cd89f1-f9f4-4cb5-a573-79809d0b6dfd.jpg?1674420350',
+      },
+    ]
+  },
+  {
+    year: 2021,
+    title: 'Tocasia\'s Dig Site',
+    body: '',
+    cards: [
+      {
+        owned: true,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/2/3/23d4b90c-95b1-4828-bc08-7067da0d5364.jpg?1674422231',
+      },
+    ]
+  },
+  {
+    year: 2021,
+    title: 'Prodigies',
+    body: '',
+    cards: [
+      {
+        owned: true,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/f/5/f5d02416-8578-40a5-bf31-d1ef9b8764f1.jpg?1674421176',
+      },
+      {
+        owned: true,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/4/4/445f897e-a059-4cd5-bfd2-302dc7a4c8e2.jpg?1674420651',
+      },
+    ]
+  },
+  {
+    year: 2021,
+    title: 'Loran, Disciple of History',
+    body: '',
+    cards: [
+      {
+        owned: true,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/9/9/9998f898-1f08-481b-8505-b890f0fec5e0.jpg?1674420216',
+      },
+    ]
+  },
+  {
+    year: 2021,
+    title: 'Take Flight + Reconstructed Thopter',
+    body: '',
+    cards: [
+      {
+        owned: true,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/4/6/46a2108e-e45e-4b21-a03b-5f644b6043be.jpg?1674420618',
+      },
+      {
+        owned: true,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/b/c/bcc8a8d8-6d8e-44b3-a29b-5fa4a851a25b.jpg?1674422018',
+      },
+    ]
+  },
+  {
+    year: 2021,
+    title: 'Cave of Koilos',
+    body: '',
+    cards: [
+      {
+        owned: true,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/9/9/99264beb-2149-4cd4-9880-f0dc5c570c1b.jpg?1673308286',
+      },
+    ]
+  },
+  {
+    year: 2021,
+    title: 'Mishra\'s Domination + Mightstone\'s Animation',
+    body: '',
+    cards: [
+      {
+        owned: true,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/8/7/87c71fb1-a30b-4e0e-a6d6-4656cfff2da1.jpg?1674421189',
+      },
+      {
+        owned: true,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/8/c/8c540e42-22e2-4127-bbd9-2e9200023fec.jpg?1674420567',
+      },
+    ]
+  },
+  {
+    year: 2021,
+    title: 'Stern Lesson',
+    body: '',
+    cards: [
+      {
+        owned: true,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/3/1/3151caa2-10ed-4f30-83eb-cd1bd3f642ce.jpg?1674420610',
+      },
+    ]
+  },
+  {
+    year: 2021,
+    title: 'Brotherhood\'s End',
+    body: '',
+    cards: [
+      {
+        owned: true,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/5/0/50f7666d-0d60-4fe5-b144-286d4e47b704.jpg?1674421089'
+      },
+    ]
+  },
+  {
+    year: 2021,
+    title: 'Overwhelming Remorse',
+    body: '',
+    cards: [
+      {
+        owned: true,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/2/0/202cbfa4-3b3d-47fd-84a6-892692c906d6.jpg?1674420959',
+      },
+    ]
+  },
+  {
+    year: 2021,
+    title: 'Machine Over Matter',
+    body: '',
+    cards: [
+      {
+        owned: true,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/d/0/d08eadf5-a86d-4e8d-b65d-79b4f88477b9.jpg?1674420560',
+      },
+    ]
+  },
+  {
+    year: 2021,
+    title: 'Jalum Tome',
+    body: '',
+    cards: [
+      {
+        owned: true,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/5/a/5a5b7c5a-ee63-4a1b-9a0f-fb0a309168df.jpg?1562913833',
+      },
+    ]
+  },
+  {
+    year: 2021,
+    title: 'Tawnos\'s Tinkering',
+    body: '',
+    cards: [
+      {
+        owned: true,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/a/c/acbd975d-52f8-4fba-bf97-b8837413edd8.jpg?1674421633',
+      },
+    ]
+  },
+  {
+    year: 2021,
+    title: 'Bitter Reunion',
+    body: '',
+    cards: [
+      {
+        owned: true,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/3/4/345a1c80-41d6-43b1-83ab-1aa56dd06b1b.jpg?1674421082',
+      },
+    ]
+  },
+  {
+    year: 2021,
+    title: 'Mishra, Tamer of Mak Fawa + The Fall of Kroog',
+    body: '',
+    cards: [
+      {
+        owned: true,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/5/8/585a753d-a692-474a-bc28-48dd93b73ace.jpg?1674421822',
+      },
+      {
+        owned: true,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/f/4/f454583d-d227-4fc8-842e-128e025880e4.jpg?1674421124',
+      },
+    ]
+  },
+  {
+    year: 2021,
+    title: 'Urza, Prince of Kroog',
+    body: '',
+    cards: [
+      {
+        owned: true,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/5/a/5a7329cd-95af-4d71-984f-f5f28982520c.jpg?1674421895',
+      },
+    ]
+  },
+  {
+    year: 2021,
+    title: 'Third Path',
+    body: '',
+    cards: [
+      {
+        owned: true,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/5/4/54fb3f4c-44fb-4afe-9fab-204ef98995c3.jpg?1674760932',
+      },
+      {
+        owned: true,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/5/f/5fcee066-425f-4341-bde5-13a5bdf06f2a.jpg?1674420515',
+      },
+    ]
+  },
+  {
+    year: 2021,
+    title: 'The sylex',
+    body: '',
+    cards: [
+      {
+        owned: false,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/8/5/856be1dd-a20b-49c2-be9d-7db76c7efd8b.jpg?1666943891',
+      },
+      {
+        owned: true,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/5/9/59faa45d-868b-4bc7-934c-0e077642e129.jpg?1674420209',
+      },
+    ]
+  },
+  {
+    year: 2021,
+    title: 'Ashnod\'s Intervention',
+    body: '',
+    cards: [
+      {
+        owned: true,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/b/c/bcf491db-d12b-4c58-9c05-085cac34a73e.jpg?1674420770'
+      },
+    ]
+  },
+  {
+    year: 2021,
+    title: 'Loran\'s Escape',
+    body: '',
+    cards: [
+      {
+        owned: false,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/3/7/3765610d-a0c1-4de9-a81b-ffa3eb06454b.jpg?1674420224',
+      },
+    ]
+  },
+  {
+    year: 2021,
+    title: 'Argoth + Gweena',
+    body: '',
+    cards: [
+      {
+        owned: true,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/b/2/b29c9e4f-7b98-4610-a681-ae6297e8fc72.jpg?1674422131',
+      },
+      {
+        owned: false,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/7/e/7ee387b7-18e4-41b7-aefe-f2b5954e3051.jpg?1674421589',
+      },
+    ]
+  },
+  {
+    year: 2021,
+    title: 'Harbin meets with Titania',
+    body: '',
+    cards: [
+      {
+        owned: true,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/d/e/deeb6a21-23b0-44f1-b70e-5899bb9d4a84.jpg?1674421647',
+      },
+      {
+        owned: true,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/5/d/5db83275-bfd2-42c2-8d0f-e8a9b4f60e78.jpg?1674421704',
+      },
+    ]
+  },
+  {
+    year: 2021,
+    title: 'Epic confrontration',
+    body: '',
+    cards: [
+      {
+        owned: true,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/8/a/8aefe8bd-216a-4ec1-9362-3f9dbf7fd083.jpg?1674421887',
+      },
+      {
+        owned: true,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/3/5/35ba6c05-a4c2-422b-a4cb-852b3aef77a5.jpg?1674421524',
+      },
+      {
+        owned: true,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/a/4/a469d7ec-42de-45de-9d8f-469bb979de58.jpg?1674421814',
+      },
+    ]
+  },
+  {
+    year: 2021,
+    title: 'Urza\'s Ruinous Blast',
+    body: '',
+    cards: [
+      {
+        owned: false,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/0/2/0220c1e9-07bc-44f0-b39e-ca345ec4ea28.jpg?1674421362',
+      },
+      {
+        owned: false,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/b/9/b9b4a02c-e57f-4287-943e-6bc859ac149e.jpg?1674420660',
+      },
+      {
+        owned: false,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/3/0/3000d1c6-dbb3-4e65-b428-dbf167bb8797.jpg?1674420431',
+      },
+      {
+        owned: true,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/5/7/57b50a96-6221-44f0-b54a-76076621047e.jpg?1562736004',
+      },
+    ]
+  },
+  {
+    year: 2021,
+    title: 'Urza\'s Spark Ignites',
+    body: '',
+    cards: [
+      {
+        owned: false,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/0/2/02aea379-b444-46a3-82f4-3038f698d4f4.jpg?1674420373',
+      },
+      {
+        owned: false,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/4/0/40a01679-3224-427e-bd1d-b797b0ab68b7.jpg?1674421988',
+      },
+    ]
+  },
+  {
+    year: 2021,
+    title: 'Gixian\'s arrive to phirexia',
+    body: '',
+    cards: [
+      {
+        owned: false,
+        scryfallId: '',
+        imgURL: 'https://cards.scryfall.io/large/front/d/0/d02b645e-ba2c-44a0-889e-c1b46d2cd925.jpg?1562784182',
+      },
+    ]
+  },
+  {
+    year: '69 A.R.',
+    title: '5 years after the war',
+    body: '',
+    cards: []
+  },
+]
