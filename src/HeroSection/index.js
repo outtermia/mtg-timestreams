2af30c76928dc28c1de136efdf2ef13627c21002
Outter Mia <outtermia@gmail.com>
2023-08-02 14:24:17 +0400

    Using Hero section as hero section

--- src/HeroSection/index.js
+++ src/HeroSection/index.js
@@ -1,46 +1,38 @@
-import {
-  Box,
-  Container,
-  Typography,
-} from '@mui/material'
-
-import { defaultSectionStyles } from 'lib/constants'
-
-import { HERO } from './content'
-
-
-const sectionStyles = {
-  // backgroundImage: 'url(/backgrounds/mock_hero.jpg)',
-  // backgroundPosition: 'center center',
-  // backgroundRepeat: 'no-repeat',
-  // backgroundSize: 'cover',
-  ...defaultSectionStyles,
-}
-
+import { Box } from '@mui/material'
 
 const HeroSection = () => (
-  <Container
+  <Box
     id="main"
     component="section"
-    fixed
-    sx={sectionStyles}
+    sx={{
+      backgroundColor: '#3f51b5',
+      position: 'relative',
+      mt: -1,
+      mx: -1,
+      py: {
+        xs: 8,  // 0px
+        sm: 10, // 600px
+        md: 13, // 900px
+        lg: 14, // 1200px
+        xl: 16, // 1536px
+      },
+    }}
   >
-    <Box>
-      <Typography variant="h1">
-        {HERO.titleA}
-      </Typography>
-      <Typography variant="h2">
-        {HERO.titleB}
-      </Typography>
-
-      <Typography
-        variant="body1"
-        sx={{ my: 3 }}
-        >
-        {HERO.body}
-      </Typography>
-    </Box>
-  </Container>
+    <Box
+      sx={{
+        backgroundImage: 'url(/backgrounds/mock_hero.jpg)',
+        backgroundPosition: 'center center',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        top: 0,
+        opacity: 0.7,
+        position: 'absolute',
+      }}
+    ></Box>
+  </Box>
 )
 
 export default HeroSection
